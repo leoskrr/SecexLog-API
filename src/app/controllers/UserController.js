e5bfdf52af3051fe2ebfe67debf2ef78a2be34ec
Leonardo <sonleo2013@gmail.com>
2019-10-13 20:22:28 -0300

    util validation and delete user added

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
+
 const { User } = require('../models');
 const { cryptPsw } = require('../utils/ProcessPassword');
+const { existsOrError, notExistsOrError } = require('../utils/validation');
 
 const Op = Sequelize.Op;
 
@@ -12,59 +14,87 @@ module.exports = {
             .catch(err => res.status(500).send(err));
     },
     //Salva um usuário
-    store(req, res) {
+    async store(req, res) {
         const user = { ...req.body };
         //fazendo a criptografia Hash da senha do usuário
-        if(user.password) user.password = cryptPsw(user.password);
+        if (user.password) user.password = cryptPsw(user.password);
+
+        try {
+            const resultFromDB = await User.findOne({
+                where: {
+                    login: user.login
+                }
+            });
+            notExistsOrError(resultFromDB, `Já existe um usuário com o login ${user.login}`)
+        } catch (msg) {
+            return res.status(400).json({ erro: msg});
+        }
 
         User.create(user)
             .then(_ => res.status(204).send())
             .catch(err => res.status(500).send(err));
     },
-    //Mostrar usuário(s) resultante(s) de pesquisa por id ou por nome
-    show(req, res) {
+    //Mostrar usuário(s) resultante(s) da pesquisa por id ou por nome
+    async show(req, res) {
         let checkById = false;
-
         const userData = req.params.data;
-        
         /*
         Verifica se o dado passado para a url é um número, para, assim, 
         fazer (ou não) uma pesquisa por id
         */
-        if(!isNaN(userData)) checkById = true; 
+        if (!isNaN(userData)) checkById = true;
 
         /* 
         Caso a condição seja verdadeira será feita uma pesquisa por id,
         retornando, assim, apenas um usuário. Caso seja falsa, será feita
         uma pesquisa por nome, retornado vários usuários. 
         */
-        if(checkById)
-        {
-            User.findOne({ 
+        if (checkById) {
+            User.findOne({
                 where: {
-                        id: userData
-                    }
+                    id: userData
+                }
             })
-            .then(user => res.json(user))
-            .catch(err => res.status(500).send(err));
+                .then(user => res.json(user))
+                .catch(err => res.status(500).send(err));
         }
-        else{
+        else {
             User.findAll({
                 where: {
                     name: { [Op.like]: `%${userData}%` }
                 }
             })
-            .then(users => res.json(users))
-            .catch(err => res.status(500).send(err));
+                .then(users => res.json(users))
+                .catch(err => res.status(500).send(err));
         }
 
     },
     //Atualiza um usuário
-    update(req, res){
+    update(req, res) {
 
     },
     //Deleta um usuário
-    delete(req, res) {
+    async delete(req, res) {
+        const userId = req.params.data;
+
+        try {
+            const resultFromDB = await User.findOne({
+                where: {
+                    id: userId
+                }
+            })
+
+            existsOrError(resultFromDB, "Não foi encontrado um usuário com o ID informado");
+
+        } catch (msg) {
+            return res.status(400).json({ erro: msg });
+        }
+
+        User.destroy({
+            where: { id: userId }
+        })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).send(err));
 
     }
 
