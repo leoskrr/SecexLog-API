7355b0aee8cc9af35e349fa0a23b4e1a503e7668
Leonardo Viana <lviana@fundacaonokia.org>
2019-10-17 12:51:48 -0400

    added delete, insert and read all

--- src/app/controllers/OpinionController.js
+++ src/app/controllers/OpinionController.js
@@ -1,21 +1,63 @@
 const Sequelize = require('sequelize');
 
 const { Opinion } = require('../models');
+const { existsOrError } = require('../utils/validation');
+
 
 module.exports = {
-    index(req,res){
 
+    index(req, res) {
+        Opinion.findAll()
+            .then(opinions => res.json(opinions))
+            .catch(err => res.status(500).send(err));
     },
-    store(req,res){
 
-    },
-    show(req,res){
+    store(req, res) {
+        //spread do formulário vindo do front-end
+        const opiniao = { ...req.body };
 
+        try {
+            existsOrError(opiniao.titulo, "O título da opinião não foi informado");
+            existsOrError(opiniao.desc, "A descrição da opinião está vazia");
+        } catch (msg) {
+            return res.status(400).json({ erro: msg });
+        }
+        Opinion.create({
+            ...opiniao,
+            title: opiniao.titulo
+        })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).send(err));
     },
-    update(req,res){
 
-    },
-    delete(req,res){
+    show(req, res) {
+
         
+
+    },
+
+    update(req, res) {
+
+    },
+
+    async delete(req, res) {
+        const { id } = req.params; 
+
+        try {
+            const resultFromDB = await Opinion.findOne({
+                where: {
+                    id,
+                }
+            })
+            existsOrError(resultFromDB, "Essa opinião não existe");
+        } catch (msg) {
+            return res.status(400).json({ erro: msg });
+        }
+
+        Opinion.destroy({
+            where: { id }
+        })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).send(err));
     }
 }
--- src/app/models/Opinion.js
+++ src/app/models/Opinion.js
@@ -1,8 +1,28 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Opinion = sequelize.define('Opinion', {
-    title: DataTypes.STRING,
-    desc: DataTypes.TEXT
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty(value){
+          if(!value){
+            throw new Error("O título não pode estar vazio");
+          }
+        }
+      }
+    },
+    desc: { 
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty(value){
+          if(!value){
+            throw new Error("A descrição não pode estar vazia");
+          }
+        }
+      }
+    }
   }, {});
   Opinion.associate = function(models) {
     // associations can be defined here
