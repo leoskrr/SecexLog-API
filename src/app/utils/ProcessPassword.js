b86ddff4cbc5923366acb2fcbaf401dd304f4195
Leonardo <sonleo2013@gmail.com>
2019-10-22 02:44:00 -0300

    Login and User/Admin authentication added

--- src/app/utils/ProcessPassword.js
+++ src/app/utils/ProcessPassword.js
@@ -3,16 +3,18 @@ const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
 /* Função usada para criptografar a senha, 
 retorna o hash da senha digitada pelo usuário */
-function cryptPsw(password)
-{
-    return bcrypt.hashSync(password, salt);
+function cryptPsw(password) {
+
+  return bcrypt.hashSync(password, salt);
+
 }
 /*Função usada para descriptografar a senha
-  Recebe a senha que está no database e a senha digitada
+  Recebe a senha digitada e a que está no database
 */
-function uncryptPsw(password, typedpass)
-{
+function uncryptPsw(typedpass, password) {
+
+  return bcrypt.compareSync(typedpass, password)
 
 }
 
-module.exports = { cryptPsw }
+module.exports = { cryptPsw, uncryptPsw }
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,39 @@
+/* CONTROLLERS */
 const UserController = require('./app/controllers/UserController');
 const OpinionController = require('./app/controllers/OpinionController');
+const LoginController = require('./app/controllers/auth/LoginController');
 
+/* MIDDLEWARES */
+const UserAuthentication = require('./app/middlewares/UserAuthentication');
+const authAdmin = require('./app/middlewares/AdminAuthentication');
+
+const AuthUser = UserAuthentication();
 
 module.exports = app => {
+   /* ROTAS DE LOGIN */
 
-    /* ROTAS DE USUÁRIOS */
+   app.post('/login', LoginController.signIn)
+   app.post('/validateToken', LoginController.validateToken);
 
-    app.route('/users')
-       .get(UserController.index)
-       .post(UserController.store);
-    
-    app.route('/users/:data')
-       .get(UserController.show)
-       .put(UserController.update)
-       .delete(UserController.delete);
-    
+   /* ROTAS DE USUÁRIOS */
+
+   app.route('/users')
+      .all(AuthUser.authenticate())
+      .get(authAdmin(UserController.index))
+      .post(authAdmin(UserController.store));
+
+   app.route('/users/:data')
+      .all(AuthUser.authenticate())
+      .get(authAdmin(UserController.show))
+      .put(authAdmin(UserController.update))
+      .delete(authAdmin(UserController.delete));
+
    /* ROTAS DE OPINIÕES */
 
    app.route('/opinions')
       .get(OpinionController.index)
       .post(OpinionController.store)
-   
+
    app.route('/opinions/:id')
       .get(OpinionController.show)
       .put(OpinionController.update)
