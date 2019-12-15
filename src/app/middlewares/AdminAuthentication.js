97a2ad4626b90efa9087113aea9258262f00e2c9
Leonardo <sonleo2013@gmail.com>
2019-12-15 10:25:39 -0300

    deleted all

--- src/app/middlewares/AdminAuthentication.js
+++
@@ -1,9 +0,0 @@
-module.exports = middleware => {
-  return (req, res, next) => {
-    if (req.user.cargo === "administrador" || req.user.cargo === "Admin") {
-      middleware(req, res, next);
-    } else {
-      res.status(401).send("Acesso somente para administradores");
-    }
-  };
-};
