b7e8097331539e2eca58eeba29b965bcececd8e8
Leonardo <sonleo2013@gmail.com>
2020-01-25 15:48:37 -0300

    type of paths changed to array

--- src/app/controllers/services/ManualSearch.js
+++ src/app/controllers/services/ManualSearch.js
@@ -1,53 +1,86 @@
 const Sequelize = require("sequelize");
 
-const { Path, City } = require("../../models");
+const { Path, City, Holiday } = require("../../models");
 const Operation = Sequelize.Op;
 
-async function getWarnings(city1, city2){
+async function getWarnings(city1, city2) {
   const warnings = [];
-  const allWarnings = await City.findAll({
-    attributes: ['obsInterdicao','obsCidade'],
-    where: {
-      nome: {
-        [Operation.in]: [city1, city2]
-      }
-    }
-  });
 
-  allWarnings.map(warning => {
-    warnings.push(warning.dataValues.obsInterdicao);
-    warnings.push(warning.dataValues.obsCidade);
-  })
+  try {
+    const allWarnings = await City.findAll({
+      attributes: ['obsInterdicao', 'obsCidade'],
+      where: {
+        nome: {
+          [Operation.in]: [city1, city2]
+        }
+      }
+    });
+
+    allWarnings.map(warning => {
+      warnings.push(warning.dataValues.obsInterdicao);
+      warnings.push(warning.dataValues.obsCidade);
+    });
+
+  } catch (error) {
+    return res.status(500).send({ "error": error });
+  }
 
   return warnings;
 }
 
-async function getPath(initCidade, endCidade, date) {
+async function getPaths(initCidade, endCidade, date) {
+  const paths = [];
 
-  const path = await Path.findOne({
-    where: {
-      initCidade: { [Operation.like]: `%${initCidade}%` },
-      endCidade: { [Operation.like]: `%${endCidade}%` }
-    }
-  })
+  try {
+    const allPaths = await Path.findAll({
+      where: {
+        initCidade: { [Operation.like]: `%${initCidade}%` },
+        endCidade: { [Operation.like]: `%${endCidade}%` }
+      }
+    })
 
-  const pathResponse = {
-    date: date,
-    arrival: {
-      time: path.dataValues.hora,
-      day: path.dataValues.dia,
-      city: path.dataValues.initCidade,
-      modal: path.dataValues.modal
-    },
-    departure: {
-      time: path.dataValues.hora,
-      day: path.dataValues.dia,
-      city: path.dataValues.endCidade,
-      modal: path.dataValues.modal
-    }
+    allPaths.map(path => {
+      paths.push({
+        date: date,
+        mileage: path.mileage,
+        price: path.cost,
+        duration: path.duration,
+        arrival: {
+          time: path.hora,
+          day: path.dia,
+          city: path.initCidade,
+          modal: path.modal
+        },
+        departure: {
+          time: path.hora,
+          day: path.dia,
+          city: path.endCidade,
+          modal: path.modal
+        }
+      });
+    })
+  } catch (error) {
+    return res.status(500).send({ "error": error });
   }
 
-  return pathResponse;
+  return paths;
+}
+
+async function formatePaths(initCidade, endCidade, dateDep, dateReg) {
+
+  const paths = [];
+
+  const pathsGoing = await getPaths(initCidade, endCidade, dateDep);
+  const pathsBack = await getPaths(endCidade, initCidade, dateReg);
+
+  for (i = 0; i < pathsGoing.length; i++) {
+    paths.push({
+      going: pathsGoing[i],
+      back: pathsBack[i]
+    })
+  }
+
+  return paths;
 }
 
 module.exports = {
@@ -57,27 +90,10 @@ module.exports = {
 
     const data = await Promise.all(object.map(async obj => {
 
-      const path = await Path.findOne({
-        where: {
-          initCidade: { [Operation.like]: `%${obj.cityDeparture}%` },
-          endCidade: { [Operation.like]: `%${obj.cityRegress}%` }
-        }
-      })
-
-      const mileage = path.dataValues.mileage;
-      const price = path.dataValues.cost;
-      const duration = path.dataValues.duration;
-
       return {
         ...obj,
-        mileage: mileage,
-        price: price,
-        duration: duration,
-        warnings: await getWarnings(obj.cityDeparture, obj.cityRegress)
-        // paths: {
-        //   going: await getPath(obj.cityDeparture, obj.cityRegress, obj.dateDeparture),
-        //   back: await getPath(obj.cityRegress, obj.cityDeparture, obj.dateRegress)
-        // }
+        warnings: await getWarnings(obj.cityDeparture, obj.cityRegress),
+        paths: await formatePaths(obj.cityDeparture, obj.cityRegress, obj.dateDeparture, obj.dateRegress)
       }
     }));
 
